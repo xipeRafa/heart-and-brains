97e6caece22a887e7f8f87228d7aecee4c7b49de
fer <fer@fers-iMac.local>
2024-09-14 14:43:08 -0700

    jij

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://xipeRafa.github.io/hearts-and-brains"
+  base: "https://xipeRafa.github.io/heart-and-brains"
 })
